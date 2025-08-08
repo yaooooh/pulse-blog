c359c51fbd898d4cc4f599940f1f2828c0ebda0a
yaooh <dahui.heay@outlook.com>
2025-08-08 12:37:19 +0800

    fix: fix antd theme  inconsistent

--- components/theme/index.tsx
+++ components/theme/index.tsx
@@ -2,6 +2,7 @@
 
 import { ThemeProvider as NextThemesProvider, useTheme } from 'next-themes';
 import { ConfigProvider, theme as antdTheme } from 'antd';
+import { useEffect, useState } from 'react';
 
 export default function ThemeWrapper({ children }: { children: React.ReactNode }) {
   return (
@@ -13,8 +14,17 @@ export default function ThemeWrapper({ children }: { children: React.ReactNode }
 
 function Inner({ children }: { children: React.ReactNode }) {
   const { resolvedTheme } = useTheme();
-  console.log(resolvedTheme)
+  const [mounted, setMounted] = useState(false);
+
   const algorithm =
-    resolvedTheme === 'dark' ? antdTheme.darkAlgorithm : antdTheme.defaultAlgorithm;
+    resolvedTheme === "dark" ? antdTheme.darkAlgorithm : antdTheme.defaultAlgorithm;
+
+  //延迟渲染 ConfigProvider，等待客户端 hydration 完成
+  useEffect(() => {
+    setMounted(true);
+  }, [resolvedTheme]);
+
+  if (!mounted) return null;
+
   return <ConfigProvider theme={{ algorithm }}>{children}</ConfigProvider>;
 }
